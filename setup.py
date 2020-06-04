f1d33bcd07e283b268519856b85fa87c8c8ab261
Gael Gentil <iamcryptoki@gmail.com>
2020-06-04 15:19:37 +0200

    Add Indonesian language.

--- setup.py
+++ setup.py
@@ -9,12 +9,13 @@ setup(
     name='passcat',
     version=passcat.__version__,
     description='Passcat lets you generate cryptographically secure, memorable passphrases.',
-    long_description=open('README.txt').read(),
-    author='Galou Gentil',
-    author_email='hello@cryptoki.fr',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    author='Gael Gentil',
+    author_email='iamcryptoki@gmail.com',
     url='https://github.com/iamcryptoki/passcat',
     license='MIT',
-    keywords='passphrase, password, security, words',
+    keywords='passphrase, password, secret, security, words',
     packages=['passcat'],
     package_data={
         'passcat' : ['wordlists/*.txt']
